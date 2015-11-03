5939330584f130243c6dd1e8cc47058047c4d5a6
Lassi Immonen <lassi.immonen@mysema.com>
2015-11-03 18:19:01 +0200

    Fix bug on replace-valeus

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mysema/datomic-tools "0.1.1-SNAPSHOT"
+(defproject mysema/datomic-tools "0.1.2"
   :description "Mysema Datomic tools"
   :url "http://mysema.com"
   :license {:name "Eclipse Public License"
